b8680a62e84439cee8b38a6398ef3dda3ae0b164
Louay Houimli <louayhouimli@engineer.com>
2024-12-27 16:38:38 +0100

    hello

--- app/page.tsx
+++ app/page.tsx
@@ -3,45 +3,51 @@ import Image from "next/image";
 import { db } from "@/db/drizzle";
 import { usersTable } from "@/db/schema";
 import DeleteBtn from "@/components/delete-btn";
-
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useForm } from "react-hook-form";
 export default async function Home() {
   const users = await db.select().from(usersTable);
   return (
-    <div>
-      <form action={createUser} className="flex flex-col gap-2">
-        <input
-          required
-          type="text"
-          name="name"
-          placeholder="Name"
-          className="border border-gray-300 rounded-md p-2"
-        />
-        <input
-          required
-          type="number"
-          name="age"
-          placeholder="Age"
-          className="border border-gray-300 rounded-md p-2"
-        />
-        <input
-          required
-          type="email"
-          name="email"
-          placeholder="Email"
-          className="border border-gray-300 rounded-md p-2"
-        />
-        <button type="submit" className="bg-blue-500 text-white rounded-md p-2">
-          Create User
-        </button>
-      </form>
-      <div>
+    <>
+      <div className="flex flex-col items-center gap-y-4  justify-center h-screen w-[500px] mx-auto">
+        <form action={createUser}>
+          <Card>
+            <CardHeader>
+              <CardTitle>Create User</CardTitle>
+              <CardDescription>Create a new user</CardDescription>
+            </CardHeader>
+            <CardContent className="flex flex-col gap-2">
+              <Input placeholder="Name" name="name" type="text" required />
+              <Input placeholder="Email" name="email" type="email" required />
+              <Input placeholder="Age" name="age" type="number" required />
+              <Button type="submit">Create</Button>
+            </CardContent>
+          </Card>
+        </form>
         {users.map((user) => (
-          <div key={user.id} className="px-2 py-1 flex items-center">
-            {user.name}
+          <div key={user.id} className="flex flex-col">
+            <div>{user.name}</div>
             <DeleteBtn id={user.id} />
           </div>
         ))}
       </div>
-    </div>
+    </>
   );
 }
