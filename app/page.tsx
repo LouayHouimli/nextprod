7e9b3afac196a112cdbca3bf2d7f37c2c4dec7b1
Louay Houimli <louayhouimli@engineer.com>
2024-12-27 13:01:45 +0100

    new haha

--- app/page.tsx
+++ app/page.tsx
@@ -8,10 +8,30 @@ export default async function Home() {
   return (
     <div>
       <form action={createUser} className="flex flex-col gap-2">
-        <input type="text" name="name" placeholder="Name" className="border border-gray-300 rounded-md p-2" />
-        <input type="number" name="age" placeholder="Age" className="border border-gray-300 rounded-md p-2" />
-        <input type="email" name="email" placeholder="Email" className="border border-gray-300 rounded-md p-2" />
-        <button type="submit" className="bg-blue-500 text-white rounded-md p-2">Create User</button>
+        <input
+          required
+          type="text"
+          name="name"
+          placeholder="Name"
+          className="border border-gray-300 rounded-md p-2"
+        />
+        <input
+          required
+          type="number"
+          name="age"
+          placeholder="Age"
+          className="border border-gray-300 rounded-md p-2"
+        />
+        <input
+          required
+          type="email"
+          name="email"
+          placeholder="Email"
+          className="border border-gray-300 rounded-md p-2"
+        />
+        <button type="submit" className="bg-blue-500 text-white rounded-md p-2">
+          Create User
+        </button>
       </form>
       <div>
         {users.map((user) => (
