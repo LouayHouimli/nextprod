7bb51b42d49ea67a47908802798488ad3dfa4382
Louay Houimli <louayhouimli@engineer.com>
2025-01-16 16:53:45 +0100

    init

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "drizzle-kit";
+import dotenv from "dotenv";
+
+// Load environment variables from .env file
+dotenv.config();
 
 export default defineConfig({
   out: "./drizzle",
   schema: "./db/schema.ts",
   dialect: "postgresql",
   dbCredentials: {
-    url: process.env.DATABASE_URL!,
+    url: process.env.DATABASE_URL!,  // Use the DATABASE_URL environment variable
   },
 });
